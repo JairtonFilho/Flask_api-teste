196a9c9f3c73460877b2058a1ff4354611437903
JairtonFilho <97057887+JairtonFilho@users.noreply.github.com>
2023-07-22 20:10:40 -0300

    fix: user.db

--- api/services/user_service.py
+++ api/services/user_service.py
@@ -4,7 +4,7 @@ from api import db
 
 def add_user(user):
     user_bd = user_model.UserModel(name=user.name, email=user.email, password=user.password)
-    user.bd.password_encrypt(user.password)
+    user_bd.password_encrypt(user.password)
     db.session.add(user_bd)
     db.session.commit()
     return user_bd
