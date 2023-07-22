b6eef4d4c023505975e93c7116ddbe5802e9eb1f
JairtonFilho <97057887+JairtonFilho@users.noreply.github.com>
2023-07-22 19:45:44 -0300

    fix: UserSchema

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -1,9 +1,9 @@
 from api import db
 
 
-class TeacherModel(db.Model):
+class UserModel(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    pasword = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
